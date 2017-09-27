63247a127671fe497048d687fa3e2c682d26ea46
Stevenson Bassy <stevebassy@gmail.com>
2017-09-27 17:31:30 -0400

    My code is a if and else statement about someone having to pay debt.

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner litScanner = new Scanner(System.in);
+
+
+        System.out.println("You better pay me $10 dollars by tomrrow or else!");
+        System.out.println( "(The next day)");
+        System.out.println("Pay up");
+        Scanner money = new Scanner(System.in);
+        int Money = money.nextInt();
+
+
+        if (Money > 1) {
+            System.out.println("That's what I thought");
+
+        } else {
+            System.out.println("You are getting the beat down!");
+        }
+
+
+    }
+}
